2b2ae31d8d7c44fd7b15aeae8ab9c92007ae4ed9
Vladimir <kynicin87@mail.ru>
2021-01-04 18:52:24 +0200

    Initial Commit

---
+++ Lesson2RepeatAllFromLesson/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lesson2RepeatAllFromLesson
+//
+//  Created by vladimir gennadievich on 04.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+
+}
+
